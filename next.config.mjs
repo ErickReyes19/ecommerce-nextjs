74821749d68b72a825d3f7c02e9b0ca7cf8874b7
ereyes0212 <erick.reyes@tiempo.hn>
2026-02-25 17:22:58 -0600

    cambios

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
           protocol: "https",
           hostname: "d3dr34vkycigpz.cloudfront.net",
         },
+        {
+          protocol: "https",
+          hostname: "es.pandora.net",
+        },
       ],
   },
   webpack: (config, { isServer }) => {
